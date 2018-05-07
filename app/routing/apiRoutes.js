61c5215eefcfbfd03b4d6dc7181e5b46ee7bf48b
Lee Warrick <leewarrick@gmail.com>
2018-05-06 20:10:34 -0400

    modularize

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,37 @@
+let friends = require('./../data/friends.js');
+//console.log(friends);
+module.exports = function(app) {
+    app.get('/api/friends', function(req, res) {
+        res.json(friends);
+    });
+    
+    app.post('/api/friends', function(req, res) {
+        let newFriend = req.body;
+        //console.log(newFriend);
+        res.json(findFriend(newFriend));
+        friends.push(newFriend);
+    });
+}
+
+function findFriend (friendObj) {
+    let answers = friendObj.answers;
+    let foundFriend;
+    let oldDif;
+    for (let i = 0; i < friends.length; i++) {
+        let newDif = 0;
+        for (let j = 0; j < friends[i].answers.length; j++) {
+            let answerA = parseInt(answers[j]);
+            let answerB = parseInt(friends[i].answers[j]);
+            let difference = Math.abs(answerA - answerB);
+            newDif += difference;
+        }//end answers loop
+        if(oldDif == undefined) {
+            oldDif = newDif;
+        }
+        if(oldDif >= newDif) {
+            oldDif = newDif;
+            foundFriend = friends[i];            
+        }
+    }//end friends loop
+    return foundFriend;
+} //end findFriend fx
